d78734da300d21899b8902c0166f29168610e945
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2026-05-20 15:55:35 +0200

    arhitectyra

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,1 +1,13 @@
-console.log('Home page loaded');
+import { getQuote } from '../api/quote';
+
+async function loadQuote() {
+  try {
+    const data = await getQuote();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loadQuote();
