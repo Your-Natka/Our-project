193dd8025ad3ed500499c4471926e606ddf5ed2c
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2026-05-20 19:31:40 +0000

    Deploying to gh-pages from @ Your-Natka/Our-project@066c8f7d176d58c5976c1ac425ae594ce3ed951a 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DLAChBYQ.js";console.log(`Favorites page loaded`);
+import"./assets/styles-DIUchJxg.js";console.log(`Favorites page loaded`);
 //# sourceMappingURL=favorites.js.map
