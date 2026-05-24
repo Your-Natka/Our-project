276c55275fbacdca433c1f2410553eb54657b287
MaksL777 <lukanenkomaks427@gmail.com>
2026-05-24 19:52:26 +0200

    fix(favorites): resolve grid gaps, tablet layout, and card alignment

--- src/render/renderModal.js
+++ src/render/renderModal.js
@@ -75,18 +75,17 @@ export function createModalMarkup(exercise, isFavorite) {
           </ul>
 
           <p class="modal-exercise-description">${description}</p>
-        </div>
-      </div>
 
-      <div class="modal-buttons">
-        <button class="modal-action-btn" type="button" data-favorites-toggle>
-          <span>${favoritesBtnText}</span>
-          <svg class="modal-action-icon" width="18" height="18">
-            <use href="./sprite.svg#${favoritesBtnIcon}"></use>
-          </svg>
-        </button>
-        <button class="modal-action-btn modal-give-rating-btn" type="button">Give a rating</button>
-      </div>
+          <div class="modal-buttons">
+            <button class="modal-action-btn" type="button" data-favorites-toggle>
+              <span>${favoritesBtnText}</span>
+              <svg class="modal-action-icon" width="18" height="18">
+                <use href="./sprite.svg#${favoritesBtnIcon}"></use>
+              </svg>
+            </button>
+            <button class="modal-action-btn modal-give-rating-btn" type="button">Give a rating</button>
+          </div>
+        </div> </div>
     </div>
   `;
 }
